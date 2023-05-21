75d2ea95a56fd46ca1364a28e23fa5a602730824
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-05-21 16:00:38 +0100

    my_commit

--- script.js
+++ script.js
@@ -41,3 +41,23 @@ popup.style.display = "block";
 localStorage.setItem("popupShown", "true");
 }
 });
+
+
+// Declare and initialize the cart count and total price variables
+let cartCount = 0;
+let totalPrice = 0;
+
+// Select all the buttons and add a click event listener to each one
+document.querySelectorAll('.product-item button').forEach(button => {
+	button.addEventListener('click', () => {
+		// Get the price of the clicked product and add it to the total price
+		const price = parseFloat(button.getAttribute('data-price'));
+		totalPrice += price;
+		
+		// Increment the cart count and display an alert message with the updated cart count and total price
+		cartCount++;
+		alert(`Item added to cart. Call 09054525949 to order
+		 Cart count: ${cartCount}. Total price: #${totalPrice.toFixed(2)
+		}`);
+	});
+});
